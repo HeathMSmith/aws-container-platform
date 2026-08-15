1ebb3b1d735a430efda9f9a368a6a1f2c626c7dc
Heath Smith <heathsmithaws2025@gmail.com>
2026-08-15 00:46:35 -0400

    feat: establish ALB foundation

--- terraform/environments/dev/network.tf
+++ terraform/environments/dev/network.tf
@@ -61,17 +61,43 @@ resource "aws_route_table_association" "public_b" {
   route_table_id = aws_route_table.public.id
 }
 
+resource "aws_security_group" "alb" {
+  name        = "aws-container-platform-${var.environment}-alb"
+  description = "Security group for the public Application Load Balancer."
+  vpc_id      = aws_vpc.app.id
+
+  ingress {
+    description = "HTTP from the internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "aws-container-platform-${var.environment}-alb"
+  }
+}
+
 resource "aws_security_group" "ecs_tasks" {
   name        = "aws-container-platform-${var.environment}-ecs-tasks"
   description = "Security group for ECS Fargate tasks."
   vpc_id      = aws_vpc.app.id
 
   ingress {
-    description = "Application traffic"
-    from_port   = 8000
-    to_port     = 8000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Application traffic from ALB"
+    from_port       = 8000
+    to_port         = 8000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb.id]
   }
 
   egress {
