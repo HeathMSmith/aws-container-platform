bc0fc4352c5ed1b2c57f8cccf0288a7b25b9cd5e
Heath Smith <heathsmithaws2025@gmail.com>
2026-08-23 18:48:15 -0400

    feat: add optional Bedrock runtime VPC endpoint

--- terraform/modules/vpc-endpoints/main.tf
+++ terraform/modules/vpc-endpoints/main.tf
@@ -49,6 +49,50 @@ resource "aws_vpc_endpoint" "logs" {
   }
 }
 
+data "aws_iam_policy_document" "bedrock_runtime_endpoint" {
+  count = var.bedrock_runtime_endpoint_enabled ? 1 : 0
+
+  statement {
+    sid    = "AllowAdvisorModelInvocation"
+    effect = "Allow"
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        var.bedrock_runtime_principal_arn,
+      ]
+    }
+
+    actions = [
+      "bedrock:InvokeModel",
+    ]
+
+    resources = [
+      var.bedrock_runtime_model_arn,
+    ]
+  }
+}
+
+resource "aws_vpc_endpoint" "bedrock_runtime" {
+  count = var.bedrock_runtime_endpoint_enabled ? 1 : 0
+
+  vpc_id              = var.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.bedrock-runtime"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = [var.private_subnet_ids[0]]
+
+  security_group_ids = [
+    var.vpc_endpoint_security_group_id,
+  ]
+
+  policy = data.aws_iam_policy_document.bedrock_runtime_endpoint[0].json
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-bedrock-runtime"
+  }
+}
+
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = var.vpc_id
   service_name      = "com.amazonaws.${var.aws_region}.s3"
